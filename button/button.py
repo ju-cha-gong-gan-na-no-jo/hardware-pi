2c4e6bcd8d4edd3d70360c77d0eeeb4be822ed05
hellojkman <94529170+hellojkman@users.noreply.github.com>
2022-04-25 14:30:19 +0900

    Update button.py

--- button/button.py
+++ button/button.py
@@ -1,10 +1,10 @@
-from gpiozero import Button               # Button 믈래스  불러오기
+from gpiozero import Button               # Button 클래스  불러오기
 from signal import pause                  #프로세스 대기함수 불러오기
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt           #mqtt 사용 라이브러리
 import json
 import time
-import pygame
-pygame.mixer.init()
+import pygame                              #음원 재생
+pygame.mixer.init()        #pygame 초기 설정 완료 무조건 해야함
 
 
 def on_connect(client, userdata, flags, rc):
@@ -22,10 +22,10 @@ def on_publish(client, userdata, mid):
 
 
 def say_hello():                #콜백함수
-    client.publish('common', json.dumps({ "success": "ok", "test": "관리실호출!!!"}), 1)
-    pygame.mixer.music.load("/home/pi/data/bell.mp3")
-    pygame.mixer.music.play()
-    time.sleep(0.2)
+    client.publish('common', json.dumps({ "success": "ok", "test": "관리실호출!!!"}), 1)        # 한글 깨짐 문제 발생 -> 받을때 json.형태로 받는 코드가 있음
+    pygame.mixer.music.load("/home/pi/data/bell.mp3")      #pygame 재생할 음원 로드
+    pygame.mixer.music.play()                   # 로드 된 파일 재생
+    time.sleep(0.2)                             # 여러번 버튼을 눌렀을때 중복 방지로 타임 슬립 줌.
 
     print("관리자 호출!!!")
 
@@ -39,9 +39,9 @@ button = Button(18)              #gpio18번 핀과 연결된 버튼을 통해 bu
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
-client.on_publish = on_publish
+client.on_publish = on_publish                    # 계속 정보를 구독자에게 계속 보냄
 
-client.connect('15.165.153.54', 1883)
+client.connect('15.165.153.54', 1883)              # 메인 ec2 주소 포트는 1883 메인  ec2는 공인 아이피기 때문에 문제 없이 정보를 날림
 client.loop_start()
 
 
